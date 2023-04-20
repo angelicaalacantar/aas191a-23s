ce99c67ece1436541d5edd3bfd8a1a399a650c60
angelicaalacantar <angelalcantar@g.ucla.edu>
2023-04-20 00:56:39 -0700

    Lab #2 Assignment

--- week2/js/init.js
+++ week2/js/init.js
@@ -1,32 +1,27 @@
-var mario = 3;
-var age = 12;
-var combine = mario + age
-console.log('Hello Asian Am 191! :)')
-
 
 // JavaScript const variable declaration
-const map = L.map('the_map').setView([34.0709, -118.444], 15); 
+const map = L.map('the_map').setView([34.0709, -118.444], 1); 
 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map); 
+}).addTo(map);
 
-//JavaScript let variable declaration to create a marker
-let marker2 = L.marker([34.0666, -118.4466]).addTo(map) 
-        .bindPopup('Ronald Reagan Medical Center')
-        .openPopup();
-let marker3 = L.marker([34.06327178477289, -118.4468590040583]).addTo(map) 
-.bindPopup('Diddy Riese')
-.openPopup();
-
-function addMarker(lat,long,message){ 
-    console.log(message) 
-    L.marker([lat,long]).addTo(map).bindPopup(message)
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    return message
 }
 
-addMarker(34.0709, -118.444, 'Math Sciences 4328')
-addMarker(34.0666, -118.4466, 'Ronald Reagan Medical Center')
-addMarker(34.06327178477289, -118.4468590040583, 'Diddy Riese')
+// use our marker functions
 
-
+    addMarker(37.005,-121.573,'Gilroy, California, USA','Home :)')
+    addMarker(10.315,123.885,'Cebu, The Philippines','2007')
+    addMarker(21.161,-86.851,'Cancun, Mexico','random location')
+    addMarker(1.352,103.819,'Singapore','2022')
+    addMarker(37.566,126.977,'Seoul, South Korea','2022')
+    addMarker(41.893, 12.468,'Rome, Italy','2019')
+    addMarker(38.716, -9.157,'Lisbon, Portugal','2019')
+    addMarker(49.281, -123.129,'Vancouver, Canada','2017')
+    addMarker(42.362, -71.064,'Boston, Massachusetts, USA','2019')
