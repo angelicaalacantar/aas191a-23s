209d067169d197b43cd45961de246c11b268d055
angelicaalacantar <angelalcantar@g.ucla.edu>
2023-05-25 02:21:17 -0700

    Lab 6 Assignment Map

--- week7/js/init.js
+++ week7/js/init.js
@@ -26,6 +26,13 @@ const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
 // add layer control box
 L.control.layers(null,layers).addTo(map)
 
+let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+    maxZoom: 16
+});
+
+Esri_WorldGrayCanvas.addTo(map);
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
